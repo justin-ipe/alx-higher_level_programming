b8de4674070f21025cf41d5709abb42c28565cd1
justin-ipe <justinmwaipe@gmail.com>
2023-08-27 07:04:13 -0700

    update

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+alph = ''.join(chr(letter) for letter in range(ord('a'), ord('z') + 1))
+print("{}".format(alph), end="")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for h in range(97, 123):
+    if h != 101 and h != 113:
+        print("{:c}".format(h), end="")
