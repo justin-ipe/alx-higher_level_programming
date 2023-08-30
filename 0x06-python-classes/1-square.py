9f46c0303ca210d46e20070e8ed2823297761d62
justin-ipe <justinmwaipe@gmail.com>
2023-08-30 02:18:32 -0700

    Mandatory tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines an empty class square"""
+
+
+class Square:
+
+    """A class defining a square"""
+
+    def __init__(self, size) -> None:
+        """
+        Instantiates class attributes
+
+        Attributes:
+             size: size of square
+        """
+        self.__size = size
